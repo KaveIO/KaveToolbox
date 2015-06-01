9872e3b1e958d26b200a69319c2b9240f3902517
rwlambert <lambert.rob@kpmg.nl>
2015-06-01 11:44:59 +0000

    Start 1.2-Beta

--- config/libInstall.py
+++ config/libInstall.py
@@ -44,7 +44,7 @@ import subprocess as sub
 #  this password is intended to be widely known and is used here as an extension of the URL
 #
 __repo_url__="http://repos:kaverepos@repos.kave.io"
-__version__="1.1-Beta"
+__version__="1.2-Beta"
 __main_dir__="KaveToolbox"
 __arch__="Centos6"
 __mirror_list_file__="/etc/kave/mirror"
