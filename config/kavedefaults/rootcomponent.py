66d05b58b7813b7eb5e67b2bcf8018dd790bd3d4
Kalin Sheytanov <ksheytanov@intracol.com>
2017-05-16 15:03:04 +0300

    modifying path to script

--- config/kavedefaults/rootcomponent.py
+++ config/kavedefaults/rootcomponent.py
@@ -22,7 +22,7 @@ root.py module: installs root on top of anaconda :)
 import os
 import sys
 import kaveinstall as li
-from kaveinstall import Component, linuxVersion, mycmd
+from kaveinstall import Component, linuxVersion, mycmd, installfrom
 from condacomponent import conda
 
 # Ubuntu14 fix libpng
@@ -52,7 +52,7 @@ glewdev.src_from = {"suffix": ".el6.x86_64.rpm"}
 class RootComponent(Component):
 
     def script(self):
-        self.run("./InstallRoot.sh")
+        self.run( installfrom + "/scripts/InstallRoot.sh" )
        
     def skipif(self):
         return (conda.installDirVersion in
@@ -89,7 +89,7 @@ root.pre = {"Centos7": ['yum -y groupinstall "Development Tools" "Development Li
                          " libjpeg-dev libtiff5-dev libxml2-dev libssl-dev libgnutls-dev libgmp3-dev libpng12-dev "
                          "libldap2-dev libkrb5-dev freeglut3-dev libfftw3-dev python-dev libmysqlclient-dev libgif-dev "
                          "libiodbc2 libiodbc2-dev libxext-dev libxmu-dev libimlib2 gccxml libxml2 libglew-dev"
-                         " glew-utils libc6-dev-i386"
+                         " glew-utils libc6-dev-i386 cmake dpkg-dev graphviz-dev"
                          ]
             }
 root.children = {"Centos6": [glew, glewdev, conda],
