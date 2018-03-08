a83ec1c45b4c63a3aaeca34351bb8eddf88d159b
Kalin Sheytanov <ksheytanov@intracol.com>
2018-03-08 15:37:30 +0200

    Fix for ABK-1508 and KTB-130
    Removing parasite spark_home assignment in KaveEnv.sh
    Putting PyGSL component not to be installed by default as it is reported
    as not needed. We need a confirmation for this.

--- config/kavedefaults/gslcomponent.py
+++ config/kavedefaults/gslcomponent.py
@@ -24,7 +24,7 @@ from kavedefaults.condacomponent import conda
 
 # ######################  pygsl 2.1 ############################
 gsl1 = Component("pygsl")
-gsl1.doInstall = True
+gsl1.doInstall = False
 gsl1.version = "2.1.1"
 gsl1.src_from = [{"arch": "noarch", "suffix": ".tar.gz"},
                  "http://downloads.sourceforge.net/project/pygsl/pygsl/pygsl-2.1.1/pygsl-2.1.1.tar.gz"]
@@ -65,7 +65,7 @@ class GslComponent(Component):
                 )
 
 gsl = GslComponent("pygsl")
-gsl.doInstall = True
+gsl.doInstall = False
 gsl.pre = {"Centos7": ["yum -y install gsl gsl-devel"],
            "Ubuntu16": ["apt-get -y install build-essential g++ libgsl0-dev gsl-bin libgl1-mesa-glx"]}
 gsl.children = {"Centos7": [gsl1],
